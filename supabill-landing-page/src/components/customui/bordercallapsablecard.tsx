b8d96dca6ea7edc94ffcbcf0df2fdf5767bf0d4f
keshav <keshav@everheal>
2024-08-06 14:31:11 +0530

    added pricing card

--- supabill-landing-page/src/components/customui/bordercallapsablecard.tsx
+++ supabill-landing-page/src/components/customui/bordercallapsablecard.tsx
@@ -5,11 +5,17 @@ type Props = {};
 
 function Bordercallapsablecard({}: Props) {
   return (
-    <div className="flex">
-      <div className="w-1/2">
+    <div className="flex my-24">
+      <div className="w-1/2 flex items-start justify-center">
         <AccordionCustom />
       </div>
-      <div className="w-1/2"></div>
+      <div className="w-1/2">
+        <img
+          src="/reports-screen-shot.png"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   );
 }
---
+++ supabill-landing-page/src/components/customui/pricingcard.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { PrimaryText } from "@/renderer/ui/primarytext";
+import React from "react";
+import ShineBorder from "../magicui/shine-border";
+import { ButtonNewYork } from "@/renderer/ui/Button";
+import { Button } from "../ui/button";
+
+type Props = {};
+
+function PricingCard({}: Props) {
+  return (
+    <section className="flex flex-col relative mx-auto mt-24 px-6 text-center py-8 my-5 md:px-8">
+      <PrimaryText title={"Pricing"} />
+      <div className="flex self-center w-[73%]">
+        <div className="w-full bg-gradient-to-br dark:from-black from-white from-30% dark:to-black/40 to-white/40 bg-clip-text py-6 text-2xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-2xl md:text-4xl lg:text-2xl translate-y-[-1rem] animate-fade-in [--animation-delay:200ms]">
+          {"One Price Fits All – No Hidden Fees, No Surprises"}
+        </div>
+      </div>
+      <div className="flex left-[400px] items-center justify-center min-h-screen absolute z-0 top-[20px]">
+        <div className="relative w-[600px] h-[600px]">
+          <div className="absolute inset-0 rounded-full bg-gradient-to-bl from-[#AF80FF] to-[#270D53] blur-[70px] opacity-35" />
+        </div>
+      </div>
+      <div className="gap-10 self-center py-3 px-5 bg-black relative z-20 flex w-[65%] flex-col items-center justify-between overflow-hidden rounded-md border border-[#FFD873] md:shadow-xl">
+        <div className="flex flex-col gap-3 items-start justify-center w-full">
+          <p className="text-[#FFD873] text-lg">
+            Supabill User (First 6 Months free)
+          </p>
+          <p className="text-[#9CA3AF]">
+            Get unlimited features, offline functionality, and top-notch support
+            - all for a single, straightforward price.
+          </p>
+        </div>
+        <div className="flex gap-2 py-4 self-start items-end justify-between">
+          <span className="text-[#3B3B3B] line-through text-xl">₹ 599</span>
+          <span className="text-white text-4xl">₹ 180/ month</span>
+          <span className="text-[#3B3B3B] text-xl">Billed annually</span>
+        </div>
+        <Button color="#F1F5F9" variant={"secondary"} className="w-full">
+          Get supabill now
+        </Button>
+
+        <div className="flex flex-wrap gap-4 py-4 w-[85%] items-center justify-start self-start">
+          {[
+            "Unlimited bill creation",
+            "Detailed report generation",
+            "Expense tracking",
+            "Hardware integration for billing",
+            "Downloadable and offline use",
+            "Quick support",
+          ].map((item, index) => {
+            return (
+              <div key={index} className="flex gap-1">
+                <img width={25} height={25} src="/check.png" alt="" />
+                <div className="text-white text-lg">{item}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PricingCard;
