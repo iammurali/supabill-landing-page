fb30717bec6c29cdc485ddbbff7303a28a13dd83
keshav <keshav@everheal>
2024-08-14 22:05:47 +0530

    added mobile fixes

--- supabill-landing-page/src/renderer/ui/NeonCard.tsx
+++ supabill-landing-page/src/renderer/ui/NeonCard.tsx
@@ -5,9 +5,9 @@ import ClientSection from "./clientlogo";
 
 export async function NeonGradientCardDemo() {
   return (
-    <div className="flex w-full items-center justify-center h-screen relative">
-      <div className="flex items-center justify-center min-h-screen absolute z-0 top-[-80px]">
-        <div className="relative w-[800px] h-[800px]">
+    <div className="flex w-full items-center justify-center h-[35vh] md:h-screen relative">
+      <div className="flex items-center justify-center min-h-[35vh] md:min-h-screen absolute z-0 top-[-80px]">
+        <div className="relative w-[300px] h-[300px] md:w-[800px] md:h-[800px]">
           <div className="absolute inset-0 rounded-full bg-gradient-to-bl from-[#AF80FF] to-[#270D53] blur-[70px] opacity-35" />
         </div>
       </div>
@@ -16,7 +16,7 @@ export async function NeonGradientCardDemo() {
         borderRadius={12}
         borderWidth={2}
         duration={20}
-        className="py-1 px-1 bg-black relative z-20 flex w-[75%] flex-col items-center justify-center overflow-hidden md:shadow-xl"
+        className="py-1 px-1 mx-4 bg-black relative z-20 flex w-full md:w-[75%] flex-col items-center justify-center overflow-hidden md:shadow-xl"
         color={["#1E1E22", "#DC4FFF", "#4361EE"]}
       >
         <span className="relative z-10 pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
@@ -27,8 +27,8 @@ export async function NeonGradientCardDemo() {
           />
         </span>
       </ShineBorder>
-      <div className="w-full z-50 h-[270px] top-[500px] absolute inset-0 bg-black from-black via-transparent to-transparent opacity-100 flex flex-col items-center justify-center">
-        <div className="mt-12 pt-8 bg-gradient-to-br dark:from-black from-white from-30% dark:to-black/40 to-white/40 bg-clip-text py-6 text-xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-2xl md:text-2xl lg:text-xl translate-y-[-1rem] animate-fade-in [--animation-delay:200ms]">
+      <div className="px-2 w-full z-50 md:h-[270px] top-[360px] md:top-[500px] absolute inset-0 bg-black from-black via-transparent to-transparent opacity-100 flex flex-col items-center justify-center">
+        <div className="md:mt-12 pt-8 bg-gradient-to-br dark:from-black from-white from-30% dark:to-black/40 to-white/40 bg-clip-text py-6 text-xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-2xl md:text-2xl lg:text-xl translate-y-[-1rem] animate-fade-in [--animation-delay:200ms]">
           Because Even Great Places Deserve Simple Billing
         </div>
         <ClientSection />
--- supabill-landing-page/src/renderer/ui/hero.tsx
+++ supabill-landing-page/src/renderer/ui/hero.tsx
@@ -7,16 +7,20 @@ export default function HeroSection({
   primaryText,
   secondaryText,
   buttonText,
-}: {primaryText: string, secondaryText: string, buttonText: string}) {
+}: {
+  primaryText: string;
+  secondaryText: string;
+  buttonText: string;
+}) {
   return (
-    <section className="flex flex-col relative mx-auto mt-24 max-w-[80rem] px-6 text-center md:px-8">
+    <section className="flex flex-col relative mx-auto md:mt-24 md:max-w-[80rem] px-2 text-center md:px-8">
       <AnimatedText title={primaryText} />
-      <div className="flex self-center w-[73%]">
-        <div className="w-full bg-gradient-to-br dark:from-black from-white from-30% dark:to-black/40 to-white/40 bg-clip-text py-6 text-2xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-2xl md:text-4xl lg:text-2xl translate-y-[-1rem] animate-fade-in [--animation-delay:200ms]">
+      <div className="flex self-end md:self-center w-full md:w-[73%]">
+        <div className="w-full text-left md:text-center bg-gradient-to-br dark:from-black from-white from-30% dark:to-black/40 to-white/40 bg-clip-text md:py-6 text-xl font-medium leading-[1.5rem] tracking-tighter text-transparent md:text-balance sm:text-2xl md:text-4xl lg:text-2xl translate-y-[-1rem] animate-fade-in [--animation-delay:200ms]">
           {secondaryText}
         </div>
       </div>
-      <div className="w-20 self-center">
+      <div className="flex md:block w-full self-end items-end md:w-20 md:self-center">
         <ButtonNewYork buttonTitle={buttonText} clickAction={() => {}} />
       </div>
     </section>
